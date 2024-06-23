d5a1b4611c963be67d7cc3050579eb73ed54b77f
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-06-23 23:35:42 +0200

    Implementing First Fit Memory Managemnt

---
+++ memory_management.py
@@ -0,0 +1,30 @@
+def firstFit(blockSize, m, processSize, n):
+
+    allocation = [-1] * n
+
+    for i in range(n):
+        for j in range(m):
+            if blockSize[j] >= processSize[i]:
+
+                allocation[i] = j
+
+                blockSize[j] -= processSize[i]
+
+                break
+
+        print("Process No. Process Size Block No")
+        for i in range(n):
+            print(" ", i + 1, "     ", processSize[i], "      ", end = "   ")
+
+            if allocation[i] != -1:
+                print(allocation[i] + 1)
+            else:
+                print("Not Allocated")
+
+if __name__ == '__main__':
+    blockSize = [100, 500, 200, 300, 600]  
+    processSize = [80, 200, 112, 345] 
+    m = len(blockSize)
+    n = len(processSize)
+
+    firstFit(blockSize, m, processSize, n)
